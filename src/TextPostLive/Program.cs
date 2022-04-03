3398f0e7987a326eca04520edc5debd3a48dbcc0
Luke Gladys <lukegladys@gmail.com>
2022-04-03 12:37:53 -0400

    Changing dev settings slightly

--- src/TextPostLive/Program.cs
+++ src/TextPostLive/Program.cs
@@ -38,6 +38,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
